1185feba0a3683fa1775eacdff0dc37dcb76864c
jjescuderv <47001432+jjescuderv@users.noreply.github.com>
2019-10-23 10:48:04 -0500

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,5 +1,5 @@
-#import mysql.connector as mysql
-conn = mysql.connector.MySQLConnection(
+import mysql.connector as mysql
+conn = mysql.MySQLConnection(
     host="evergreen.mysql.database.azure.com",
     port=3306,
     user="evergreenadmin@evergreen",
