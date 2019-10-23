d163d108570c5d9a25552a1bdef40e6d941d3b6f
jjescuderv <47001432+jjescuderv@users.noreply.github.com>
2019-10-23 15:09:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-
+from flask_cors import CORS
 from controllers.Indicadores import Indicador
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/indicadores', methods=['GET'])
 def getAll():
